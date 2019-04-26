21d6a4d95264f57779077b8a2bd0184fcd13cba2
darkdoct <ninjahousecat@gmail.com>
2019-04-26 09:34:44 -0400

    rebuild

--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -7,22 +7,20 @@ import java.sql.SQLException;
 public class ConnectionFactory {
 
 	// Private Constructor to enforce usage of factory method
-	private ConnectionFactory() {}
+	//private ConnectionFactory() {}
 	
 	public static Connection getConnection() {
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-			return DriverManager.getConnection(System.getenv("JDBC_URL"), 
-											   System.getenv("JDBC_USERNAME"), 
-											   System.getenv("JDBC_PASSWORD"));
-		} catch (SQLException e) {
-			System.err.println("Error Code: " + e.getErrorCode());
-			System.err.println("SQL State: " + e.getSQLState());
-			System.err.println("Message: " + e.getMessage());
-			throw new RuntimeException("Failed to get database connection");
-		} catch (ClassNotFoundException e) {
-			System.err.println("Failed to locate JDBC Driver");
-			throw new RuntimeException("Failed to locate JDBC driver");
-		}
+	    try {
+	        // This line is what did the trick for me locally
+	        Class.forName("oracle.jdbc.OracleDriver");
+	        return DriverManager.getConnection(System.getenv("JDBC_URL"), System.getenv("JDBC_USERNAME"), System.getenv("JDBC_PASSWORD"));
+	    } catch (SQLException e) {
+	        System.err.println("SQL State: " + e.getSQLState());
+	        System.err.println("Error Code: " + e.getErrorCode());
+	        System.err.println("Reason: " + e.getMessage());
+	        throw new RuntimeException("Failed to obtain database connection");
+	    } catch (ClassNotFoundException e) {
+	        throw new RuntimeException("Failed to load JDBC Driver");
+	    }
 	}
 }
